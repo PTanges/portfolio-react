11a31fcd6bf60236d3018f6b0e3a26247dd048cc
Patrick <pattontanges@gmail.com>
2025-01-02 16:59:02 -0800

    Removed initial files & initial website created with hello world

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import Class from './File.jsx';
+
+function App() {
+  return (
+    <div id="container">
+      <p>"Hello world!"</p>
+
+      {/* <div id="content" class="content_section">
+
+      </div>
+
+      <div id="footer" class="footer_section">
+
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
