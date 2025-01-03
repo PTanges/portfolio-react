df5ecc0d757a2d7b2c4063ec1124eb178214e217
Patrick <pattontanges@gmail.com>
2025-01-02 17:46:24 -0800

    Initial Framework for navigation sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@
 function App() {
   return (
     <div id="container">
-      <p>"Hello world!"</p>
+      <div id="header" class="header_section">
+        <h2 className="header_title">
+            HOME
+        </h2>
 
-      {/* <div id="content" class="content_section">
+        <ul class="nav-bar">
+            <li id="nav_home"><button>Home</button></li>
+            <li id="nav_biography"><button>Biography</button></li>
+            <li id="nav_projects"><button>Projects</button></li>
+        </ul>
+      </div>
+      {/* end header */}
+
+      <div id="content" class="content_section">
 
       </div>
+      {/* end content */}
 
       <div id="footer" class="footer_section">
 
-      </div> */}
-    </div>
+      </div>
+      {/* end footer */}
+    </div> // end container
   );
 }
 
